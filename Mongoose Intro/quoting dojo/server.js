032d5fc81384c3de6206f0941f8d0c4efcfdffb7
Muhammad <nhmishaq@gmail.com>
2018-01-02 17:13:57 -0600

    cleaned up quoting dojo

--- Mongoose Intro/quoting dojo/server.js
+++ Mongoose Intro/quoting dojo/server.js
@@ -15,65 +15,35 @@ mongoose.Promise = global.Promise;
 
 var UserSchema = new mongoose.Schema({
     name: {type: String, required: true, minlength: 6},
-    quote: {type: Number, min: 1, max: 150}
+    quote: {type: String,required: true, minlength: 6}
    }, {timestamps: true});
 
-   mongoose.model('User', UserSchema); // We are setting this Schema in our Models as 'User'
-   var User = mongoose.model('User')
+   mongoose.model('Users', UserSchema); // We are setting this Schema in our Models as 'User'
+   var User = mongoose.model('Users')
 
 
 // Routes
 // Root Request
 app.get('/', function(req, res) {
-    User.find({}, function(err, users) {
-
-        if(err) {
-            res.render('index', {errors: user.errors})
-        } else { // else console.log that we did well and then redirect to the root route
-          console.log('successfully added a user!');
-          res.render('index', {users: users});
-        }
-
-        // This is the method that finds all of the users from the database
-        // Notice how the first parameter is the options for what to find and the second is the
-        //   callback function that has an error (if any) and all of the users
-        // Keep in mind that everything you want to do AFTER you get the users from the database must
-        //   happen inside of this callback for it to be synchronous 
-        // Make sure you handle the case when there is an error, as well as the case when there is no error
-      })
-    // res.render('index', {User: users});
+    res.render('index');
 })
 // Add User Request 
-app.post('/quotes', function(req, res) {
-    console.log("POST DATA", req.body);
-    // This is where we would add the user from req.body to the database.
-    var user = new User(req.body);
-
-    user.save(function(err) {
-        // if there is an error console.log that something went wrong!
-        if(err) {
-            res.render('index', {errors: user.err})
-        } else { // else console.log that we did well and then redirect to the root route
-          console.log('successfully added a user!');
-          res.render('quotes');
+app.post('/quotes', function(req, res) {    
+    User.create(req.body, function(err) {
+        if (err){
+            console.log(err);
         }
-      })    
-    
+        res.redirect('quotes');
+    })
 })
-
 app.get('/quotes', function(req, res) {
     User.find({}, function(err, users) {
-
-        if(err) {
-            res.render('quotes', {errors: user.errors})
-        } else { // else console.log that we did well and then redirect to the root route
-          console.log('successfully added a user!');
-          res.render('quotes', {users: users});
+        if (err){
+            console.log(err);
         }
+        res.render('quotes', {users: users});
       })
-    // res.render('index', {User: users});
 })
-
 // Setting our Server to Listen on Port: 8000
 app.listen(8000, function() {
     console.log("listening on port 8000");
